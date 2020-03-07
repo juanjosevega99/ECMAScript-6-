ad1319c19a148e802ec224abe0b9b3337f9f63f1
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-07 10:38:01 -0500

    Clases, Módulos y Generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -69,3 +69,37 @@ helloPromise()
   .then(response => console.log(response))
   .then(() => console.log("Hola"))
   .catch(error => console.log(error));
+
+//Clases, Módulos y Generadores
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import { hello } from "./module";
+
+hello();
+
+function* helloWorld() {
+  if (true) {
+    yield "Hello, ";
+  }
+  if (true) {
+    yield "World";
+  }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
