5d6477f2b8ba14a9e49acc6da7ef1a937f03cb1b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-07 09:52:40 -0500

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -11,3 +11,25 @@ let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem = `frase epica
+ahora es otra frase epica
+`;
+
+console.log(lorem);
+
+let person = {
+  name: "juan",
+  age: 20,
+  country: "CO"
+};
+
+let { name, age } = person;
+console.log(name, age);
+
+let team1 = ["Juan", "Ana", "Car"];
+let team2 = ["Camila", "Maria", "Kim"];
+
+let education = ["David", ...team1, ...team2];
+
+console.log(education);
